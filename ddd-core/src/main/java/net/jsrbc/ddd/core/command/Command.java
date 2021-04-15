9f9aaad9d414979cc4fbca425b7ce2b437e6a3fb
ZZZ <460427679@qq.com>
2021-04-15 17:23:46 +0800

    命令对象补充返回值

--- ddd-core/src/main/java/net/jsrbc/ddd/core/command/Command.java
+++ ddd-core/src/main/java/net/jsrbc/ddd/core/command/Command.java
@@ -5,10 +5,11 @@ package net.jsrbc.ddd.core.command;
  * @author ZZZ on 2020/8/4 21:16
  * @version 1.0
  */
-public interface Command<RECEIVER> {
+public interface Command<RECEIVER, RESULT> {
     /**
      * 执行命令
      * @param receiver 接收者
+     * @return 执行完成后，返回的结果
      */
-    void execute(RECEIVER receiver);
+    RESULT execute(RECEIVER receiver);
 }
