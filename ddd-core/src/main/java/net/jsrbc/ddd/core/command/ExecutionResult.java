dc3b4abc1bb08a2a9e6b451455788d1f39cbebf1
ZZZ <460427679@qq.com>
2021-04-16 09:31:30 +0800

    修改命令执行结果的构造方法

--- ddd-core/src/main/java/net/jsrbc/ddd/core/command/ExecutionResult.java
+++ ddd-core/src/main/java/net/jsrbc/ddd/core/command/ExecutionResult.java
@@ -1,5 +1,7 @@
 package net.jsrbc.ddd.core.command;
 
+import net.jsrbc.ddd.core.model.aggregate.Aggregate;
+
 /**
  * 命令操作执行结果
  * @author ZZZ on 2021/4/15 20:33
@@ -13,14 +15,32 @@ public class ExecutionResult {
     /** 执行完后的版本号 */
     private final Long version;
 
-    public ExecutionResult(String id) {
-        this.id = id;
-        this.version = null;
+    /**
+     * 通过聚合构造执行结果
+     * @param aggregate 聚合
+     * @return 执行结果
+     */
+    public static ExecutionResult of(Aggregate aggregate) {
+        return new ExecutionResult(aggregate.getId(), aggregate.getVersion());
     }
 
-    public ExecutionResult(String id, Long version) {
-        this.id = id;
-        this.version = version;
+    /**
+     * 通过ID构造执行结果
+     * @param id 聚合ID
+     * @return 执行结果
+     */
+    public static ExecutionResult of(String id) {
+        return new ExecutionResult(id, null);
+    }
+
+    /**
+     * 通过ID和版本号构造结果
+     * @param id 聚合ID
+     * @param version 版本号
+     * @return 执行结果
+     */
+    public static ExecutionResult of(String id, Long version) {
+        return new ExecutionResult(id, version);
     }
 
     public String getId() {
@@ -29,5 +49,10 @@ public class ExecutionResult {
 
     public Long getVersion() {
         return version;
+    }
+
+    private ExecutionResult(String id, Long version) {
+        this.id = id;
+        this.version = version;
     }
 }
