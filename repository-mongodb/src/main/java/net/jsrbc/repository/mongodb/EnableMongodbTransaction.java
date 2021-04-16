a9b90f26d2e463385780b4cb96e5c560e77dd02a
ZZZ <460427679@qq.com>
2021-04-16 15:47:18 +0800

    补充mongodb事务

---
+++ repository-mongodb/src/main/java/net/jsrbc/repository/mongodb/EnableMongodbTransaction.java
@@ -0,0 +1,18 @@
+package net.jsrbc.repository.mongodb;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 启用mongodb事务
+ * @author ZZZ on 2021/4/16 15:27
+ * @version 1.0
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Import(MongoTransactionConfigurer.class)
+public @interface EnableMongodbTransaction {}
---
+++ repository-mongodb/src/main/java/net/jsrbc/repository/mongodb/MongoTransactionConfigurer.java
@@ -0,0 +1,28 @@
+package net.jsrbc.repository.mongodb;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+/**
+ * mongodb事务配置
+ * @author ZZZ on 2021/4/16 15:29
+ * @version 1.0
+ */
+@ConditionalOnBean(MongoConfigure.class)
+@Configuration
+public class MongoTransactionConfigurer {
+
+    @Bean
+    public MongoTransactionManager mongoTransactionManager(MongoDatabaseFactory mongoDatabaseFactory) {
+        return new MongoTransactionManager(mongoDatabaseFactory);
+    }
+
+    @Bean
+    public TransactionExecutor transactionExecutor(MongoTemplate mongoTemplate, MongoTransactionManager mongoTransactionManager) {
+        return new TransactionExecutor(mongoTemplate, mongoTransactionManager);
+    }
+}
